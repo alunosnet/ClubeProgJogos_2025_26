294786f8bb2e9b569fa3435be449868acb4f300d
User <alunosnet@gmail.com>
2025-11-19 15:55:29 +0000

    Empurrar bolas

--- Assets/Scripts/Atirar.cs
+++ Assets/Scripts/Atirar.cs
@@ -4,6 +4,8 @@ public class Atirar : MonoBehaviour
 {
     public GameObject ModeloBola;
     public Transform PosicaoAtirar; //preencher no inspector
+    public float forcaAtirar = 10;
+    public float tempoVidaBola = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,8 +18,13 @@ public class Atirar : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             //instanciar bola
-            Instantiate(ModeloBola, PosicaoAtirar.position, Quaternion.identity);
-
+            GameObject bola=Instantiate(ModeloBola, PosicaoAtirar.position, Quaternion.identity);
+            //Referência para o componente rigidbody da bola
+            Rigidbody rb = bola.GetComponent<Rigidbody>();
+            //aplicar uma força na bola
+            rb.AddForce(transform.forward * forcaAtirar);
+            //destroi a bola ao fim de x segundos
+            Destroy(bola, tempoVidaBola);
         }
     }
 }
